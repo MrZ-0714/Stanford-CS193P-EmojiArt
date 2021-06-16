89daada84091f69289eef3ce849dc0ed719c5890
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-06-16 15:06:12 -0700

    Initial commit.

---
+++ Stanford-CS193P-EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,12 @@
+//
+//  EmojiArtDocument.swift
+//  Stanford-CS193P-EmojiArt
+//
+//  Created by Zimo Zhao on 6/16/21.
+//
+
+import Foundation
+
+class EmojiArtDocument: ObservableObject {
+    static let palette: String = "EMOJIS"
+}
