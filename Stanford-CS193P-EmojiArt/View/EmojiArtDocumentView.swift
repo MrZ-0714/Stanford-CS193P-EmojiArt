0385005f38e3f83894f6e2025d5e3c4add9c4a78
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-06-17 16:19:17 -0700

    Adjust image size with double tap gesture.

--- Stanford-CS193P-EmojiArt/View/EmojiArtDocumentView.swift
+++ Stanford-CS193P-EmojiArt/View/EmojiArtDocumentView.swift
@@ -25,37 +25,59 @@ struct EmojiArtDocumentView: View {
             
             GeometryReader { geometry in
                 ZStack {
-                    Rectangle()
-                        .foregroundColor(.white)
-                        .overlay(
-                            Group {
-                                if self.document.backgroundImage != nil {
-                                    Image(uiImage: self.document.backgroundImage!)
-                                }
-                            }
-                        )
-                        .edgesIgnoringSafeArea(.bottom)
-                        .onDrop(of: ["public.image", "public.text"], isTargeted: nil) { providers, location in
-                            var location = geometry.convert(location, from: .global)
-                            location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2 )
-                            return self.drop(providers: providers, at: location)
-                        }
+                    Color.white.overlay(
+                        OptionalImage(uiImage: self.document.backgroundImage)
+                            .scaleEffect(self.zoomScale)
+                    )
+                    .gesture(self.doubleTapToZoom(in: geometry.size))
+                    
                     ForEach(self.document.emojis) { emoji in
                         Text(emoji.text)
                             .font(self.font(for: emoji))
                             .position(self.position(for: emoji, in: geometry.size))
                     }
                 }
+                .clipped()
+                .edgesIgnoringSafeArea([.horizontal, .bottom])
+                .onDrop(of: ["public.image", "public.text"], isTargeted: nil) { providers, location in
+                    var location = geometry.convert(location, from: .global)
+                    location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2 )
+                    location = CGPoint(x: location.x / zoomScale, y: location.y / zoomScale )
+                    return self.drop(providers: providers, at: location)
+                }
             }
         }
     }
-        
+    
+    @State private var zoomScale: CGFloat = 1.0
+    
+    private func doubleTapToZoom(in size: CGSize) -> some Gesture {
+        TapGesture(count: 2)
+            .onEnded{
+                withAnimation {
+                    self.zoomToFit(self.document.backgroundImage, in: size)
+                }
+            }
+    }
+    
+    private func zoomToFit(_ image: UIImage?, in size: CGSize) {
+        if let image = image, image.size.width > 0, image.size.height > 0 {
+            let hZoom = size.width / image.size.width
+            let vZoom = size.height / image.size.height
+            
+            self.zoomScale = min(hZoom, vZoom)
+        }
+    }
+    
     private func font(for emoji: EmojiArt.Emoji) -> Font {
-        Font.system(size: emoji.fontSize)
+        Font.system(size: emoji.fontSize * zoomScale)
     }
     
     private func position(for emoji: EmojiArt.Emoji, in size: CGSize) -> CGPoint {
-        CGPoint(x: emoji.location.x + size.width/2, y: emoji.location.y + size.height/2)
+        var location = emoji.location
+        location = CGPoint(x: location.x * zoomScale, y: location.y * zoomScale)
+        location = CGPoint(x: location.x + size.width/2, y: location.y + size.height/2)
+        return location
     }
     
     private func drop(providers: [NSItemProvider], at location: CGPoint) -> Bool {
@@ -71,6 +93,18 @@ struct EmojiArtDocumentView: View {
     }
     
     private let defaultEmojiSize: CGFloat = 40
+}
+
+struct OptionalImage: View {
+    var uiImage: UIImage?
+    
+    var body: some View {
+        Group {
+            if uiImage != nil {
+                Image(uiImage: uiImage!)
+            }
+        }
+    }
 }
 
 //extension String: Identifiable {
