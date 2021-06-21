5b3a5985e34b60134bd2a69e42b3fe7e98412d68
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-06-21 12:45:06 -0700

    bug fix for zoom gesture.

--- Stanford-CS193P-EmojiArt/View/EmojiArtDocumentView.swift
+++ Stanford-CS193P-EmojiArt/View/EmojiArtDocumentView.swift
@@ -11,12 +11,12 @@ struct EmojiArtDocumentView: View {
     @ObservedObject var document: EmojiArtDocument
     
     var body: some View {
-        VStack() {
+        VStack {
             ScrollView(.horizontal) {
-                HStack() {
+                HStack {
                     ForEach(EmojiArtDocument.palette.map { String($0) }, id: \.self) { emoji in
                         Text(emoji)
-                            .font(Font.system(size: 40))
+                            .font(Font.system(size: defaultEmojiSize))
                             .onDrag{ return NSItemProvider(object: emoji as NSString) }
                     }
                 }
@@ -39,11 +39,11 @@ struct EmojiArtDocumentView: View {
                     }
                 }
                 .clipped()
-                .edgesIgnoringSafeArea([.horizontal, .bottom])
                 .gesture(panGesture())
                 .gesture(zoomGesture())
+                .edgesIgnoringSafeArea([.horizontal, .bottom])
                 .onDrop(of: ["public.image", "public.text"], isTargeted: nil) { providers, location in
-                    var location = geometry.convert(location, from: .global)
+                    var location = CGPoint(x: location.x, y: geometry.convert(location, from: .global).y)
                     location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2 )
                     location = CGPoint(x: location.x - panOffset.width, y: location.y - panOffset.height)
                     location = CGPoint(x: location.x / zoomScale, y: location.y / zoomScale )
@@ -68,7 +68,8 @@ struct EmojiArtDocumentView: View {
                 gestureZoomScale = latestGestureScale
             }
             .onEnded { finalGestureScale in
-                steadyStateZoomScale += finalGestureScale
+                print(finalGestureScale)
+                steadyStateZoomScale *= finalGestureScale
             }
     }
     
