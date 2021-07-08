a18eb1d727eb85cb0360c88e80621a7be626ebd3
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-07-07 17:11:10 -0700

    view modifier

--- Stanford-CS193P-EmojiArt/View/PaletteChoose.swift
+++ Stanford-CS193P-EmojiArt/View/PaletteChoose.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct PaletteChooser: View {
     @ObservedObject var document: EmojiArtDocument
-    @Binding var chosenPalette: String 
+    @Binding var chosenPalette: String
+    @State private var showPaletteEditor: Bool = false
         
     var body: some View {
         HStack {
@@ -19,8 +20,56 @@ struct PaletteChooser: View {
                 chosenPalette = document.palette(before: chosenPalette)
             }, label: { EmptyView()} )
             Text(document.paletteNames[chosenPalette] ?? "")
+            Image(systemName: "keyboard").imageScale(.large)
+                .onTapGesture {
+                    showPaletteEditor = !showPaletteEditor
+                }
+                .popover(isPresented: $showPaletteEditor)  {
+                    PaletteEditor(chosenPalette: $chosenPalette)
+                        .environmentObject(document)
+                        .frame(minWidth: 300, minHeight: 500)
+                }
         }
         .fixedSize(horizontal: true, vertical: false)
     }
 }
 
+struct PaletteEditor: View {
+    @EnvironmentObject var document: EmojiArtDocument
+    @Binding var chosenPalette: String
+    @State private var palletteName: String = ""
+    @State private var emojisToAdd: String = ""
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Text("Palette Editor for: ").font(.headline).padding()
+            Text(document.paletteNames[chosenPalette] ?? "")
+            Divider()
+            Form {
+                TextField("Palette Name", text: $palletteName, onEditingChanged: {
+                    began in
+                    if !began {
+                        document.renamePalette(chosenPalette, to: palletteName)
+                    }
+                })
+                TextField("Add Emoji", text: $emojisToAdd, onEditingChanged: {
+                    began in
+                    if !began {
+                        chosenPalette = document.addEmoji(emojisToAdd, toPalette: chosenPalette)
+                        emojisToAdd = ""
+                    }
+                })
+                Section(header: Text("Remove Emojis")) {
+                    VStack {
+                        ForEach(chosenPalette.map{ String($0) }, id: \.self) { emoji in
+                            Text(emoji).onTapGesture {
+                                chosenPalette = document.removeEmoji(emoji, fromPalette: chosenPalette)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .onAppear {palletteName = document.paletteNames[chosenPalette] ?? ""}
+    }
+}
