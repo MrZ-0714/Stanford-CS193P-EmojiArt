dc99f9ced7f5d7a85ffc9982dc47ac55c69db615
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-07-12 17:19:07 -0700

    Add Editable button to change text label for each document.

--- Stanford-CS193P-EmojiArt/View/EmojiArtDocumentChooser.swift
+++ Stanford-CS193P-EmojiArt/View/EmojiArtDocumentChooser.swift
@@ -9,16 +9,19 @@ import SwiftUI
 
 struct EmojiArtDocumentChooser: View {
     @EnvironmentObject var store: EmojiArtDocumentStore
+    @State private var editMode: EditMode = .inactive
     
     var body: some View {
         NavigationView {
             List {
                 ForEach(store.documents) { document in
                     NavigationLink(destination: EmojiArtDocumentView(document: document)
-                            .navigationBarTitle(self.store.name(for: document))
+                                    .navigationBarTitle(self.store.name(for: document))
                     ) {
-                        Text(store.name(for: document))
+                        EditableText( store.name(for: document), isEditing: editMode.isEditing) { name in
+                            store.setName(name, for: document)
                         }
+                    }
                 }
                 .onDelete { indexSet in
                     indexSet.map{self.store.documents[$0]}.forEach{ document in
@@ -31,11 +34,11 @@ struct EmojiArtDocumentChooser: View {
                 leading: Button(
                     action: { self.store.addDocument() },
                     label: { Image(systemName: "plus").imageScale(.large) }
-                )
+                ),
+                trailing: EditButton()
             )
         }
-
-
+        .environment(\.editMode, $editMode)
     }
 }
 
